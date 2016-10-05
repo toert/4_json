49ca9509d1545ac74f9113e397ee3b18add6bb72
toert <pw177@mail.ru>
2016-10-05 19:10:45 +0400

    на проверку

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,19 @@
 import json
+import pprint
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r', encoding = 'utf-8') as data:
+        return json.load(data) 
 
 
 def pretty_print_json(data):
-    pass
+    for i in range(len(data)):
+        pprint.pprint(data[i])
+   
 
 
 if __name__ == '__main__':
-    pass
+    filepath_to_json = input('Enter filepath to your json: ')
+    text_from_file = load_data(filepath_to_json)
+    pretty_print_json(text_from_file)
